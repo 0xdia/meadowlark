faeadffa59179b9af122dca026747cfc28a334c4
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2021-10-26 17:43:09 +0200

    add static middleware

--- meadowlark.js
+++ meadowlark.js
@@ -3,12 +3,14 @@ const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
 }))
 app.set('view engine', 'handlebars')
 
-const port = process.env.PORT || 3000
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => res.render('home'))
 
