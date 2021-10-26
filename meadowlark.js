c7bb1d8ba52e8cc214f32842da06bdd4da91d8ad
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2021-10-26 17:30:43 +0200

    install handlebars view engine, add views

--- meadowlark.js
+++ meadowlark.js
@@ -1,30 +1,28 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Meadlark Travel')
-})
+app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => {
-    res.type('text/plain')
-    res.send('About Meadlark Travel')
-})
+app.get('/about', (req, res) => res.render('about'))
 
 app.use((req, res) => {
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - Not Found')
+    res.render('404')
 })
 
 app.use((err, req, res, next) => {
     console.log(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - Server Error')
+    res.render('500')
 })
 
 app.listen(port, () => console.log(
